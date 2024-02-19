4bb795051a383167f7122cad09f4aa8aeb59989b
aquarock <aqua.rock.slug@gmail.com>
2024-02-19 10:57:14 -0600

    init

---
+++ init.vim
@@ -0,0 +1,52 @@
+set number
+call plug#begin('~/.local/share/nvim/site/plugged')
+Plug 'nvim-lua/plenary.nvim'
+Plug 'm4xshen/autoclose.nvim'
+Plug 'startup-nvim/startup.nvim',
+Plug 'junegunn/goyo.vim'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'sbdchd/neoformat'
+Plug 'neovim/nvim-lspconfig'
+Plug 'nvim-treesitter/nvim-treesitter'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'NvChad/nvim-colorizer.lua'
+Plug 'sitiom/nvim-numbertoggle'
+Plug 'numToStr/Comment.nvim'
+Plug 'mfussenegger/nvim-lint'
+Plug 'errata-ai/vale'
+Plug 'akinsho/toggleterm.nvim'
+Plug 'jesseduffield/lazygit'
+" Plug 'mfussenegger/nvim-dap'
+" Plug 'rcarriga/nvim-dap-ui'
+" Plug 'mfussenegger/nvim-dap-python'
+" Plug 'folke/neodev.nvim'
+Plug 'rinx/nvim-ripgrep'
+Plug 'nvim-telescope/telescope-file-browser.nvim'
+Plug 'tree-sitter/tree-sitter'
+Plug 'tree-sitter/tree-sitter-html'
+Plug 'luckasRanarison/nvim-devdocs'
+Plug 'habamax/vim-godot'
+Plug 'kristijanhusak/vim-carbon-now-sh'
+Plug 'tadmccorkle/markdown.nvim'
+call plug#end()
+
+map <C-h> :wincmd h<CR>
+map <C-j> :wincmd j<CR>
+map <C-k> :wincmd k<CR>
+map <C-l> :wincmd l<CR>
+
+map <F1> :Neoformat<CR> :w<CR>
+map <F2> :lua _lazygit()<CR>
+map <F3> :Telescope current_buffer_fuzzy_find<CR>
+map <F4> :Telescope treesitter<CR>
+map <F5> :TermExec direction=vertical dir=./ border=double size=70 cmd=''<CR>
+map <F6> :DevdocsOpenCurrent<CR>
+map <F7> :ToggleTerm direction=horizontal dir=./ border=double size=20<CR>
+map <F8> :ToggleTerm direction=vertical dir=./ border=double size=70<CR>
+
+map <F10> :Goyo<CR>
+" map <F11> fullscreen
+map <F12> :wq<CR>
+
+let g:airline_theme='soda'
---
+++ plugin/init.lua
@@ -0,0 +1,126 @@
+require('telescope').setup()
+require("nvim-treesitter").setup()
+require('markdown').setup()
+
+require("nvim-devdocs").setup()
+-- require('dap-python').setup('/usr/bin/python')
+-- require('dapui').setup()
+require('Comment').setup()
+require("startup").setup({theme = "dashboard"})
+
+require("toggleterm").setup{
+	shade_terminals = true,
+	float_opts = {
+		border = 'curved',
+		height = 5,
+
+	}
+}
+-- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({
+	cmd = "lazygit",
+	direction = "float",
+	float_opts = {
+		border = 'double',
+		height = 40
+	},
+	hidden = true,
+})
+function _lazygit()
+  lazygit:toggle()
+end
+
+local config = {
+   keys = {
+      ["("] = { escape = false, close = true, pair = "()" },
+      ["["] = { escape = false, close = true, pair = "[]" },
+      ["{"] = { escape = false, close = true, pair = "{}" },
+
+      [">"] = { escape = true, close = false, pair = "<>" },
+      [")"] = { escape = true, close = false, pair = "()" },
+      ["]"] = { escape = true, close = false, pair = "[]" },
+      ["}"] = { escape = true, close = false, pair = "{}" },
+
+      ['"'] = { escape = true, close = true, pair = '""' },
+      ["'"] = { escape = true, close = true, pair = "''" },
+      ["`"] = { escape = true, close = true, pair = "``" },
+   },
+   options = {
+      disabled_filetypes = { "text" },
+      disable_when_touch = false,
+      touch_regex = "[%w(%[{]",
+      pair_spaces = false,
+      auto_indent = true,
+   },
+}
+require("autoclose").setup(config)
+
+require('lint').linters_by_ft = {
+  -- javascript = {'quick-lint-js'},
+  python = {'pylint'},
+  json = {'jsonlint'},
+  markdown = {'vale'},
+  lua = {'luacheck'}
+}
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    require("lint").try_lint()
+  end,
+})
+
+-- Setup language servers.
+local lspconfig = require('lspconfig')
+lspconfig.pyright.setup {}
+lspconfig.tsserver.setup {}
+lspconfig.rust_analyzer.setup {
+  -- Server-specific settings. See `:help lspconfig-setup`
+  settings = {
+    ['rust-analyzer'] = {},
+  },
+}
+
+-- search
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
