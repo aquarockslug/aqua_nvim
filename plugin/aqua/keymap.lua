1d61949df9d134c90e8b5e6d791667c58f9f409a
zach <zach.lavake@merydyan.com>
2024-07-25 04:25:27 -0500

    cleaned functions

--- plugin/aqua/keymap.lua
+++ plugin/aqua/keymap.lua
@@ -1,39 +1,26 @@
 -- AQUA ARCH NEOVIM KEYMAP
 local vim = vim -- avoid undefined var warning
-local Terminal = require'toggleterm.terminal'.Terminal
 
 vim.keymap.set('n', 'U', '<C-r>') -- undo
 
 -- FUNCTION KEYS
--- F1
-local lazygit = Terminal:new({cmd = 'lazygit', direction = 'float'})
-vim.keymap.set('n', '<F1>', function() lazygit:toggle() end)
-
--- F2
-vim.keymap.set('n', '<F2>', ':Neoformat<CR> :w<CR>')
-
--- F3
-local web_search = Terminal:new({
-    cmd = 'ddgr --rev',
-    direction = 'float'
-})
-vim.keymap.set('n', '<F3>', function() web_search:toggle() end)
-
--- F4
-local buku = Terminal:new({cmd = 'oil', direction = 'float'})
-vim.keymap.set('n', '<F4>', function() buku:toggle() end)
-
--- F5
-local nap = Terminal:new({cmd = 'nap', direction = 'float'})
-vim.keymap.set('n', '<F5>', function() nap:toggle() end)
+local Terminal = require'toggleterm.terminal'.Terminal
+local floater = function(cmd) return Terminal:new({cmd = cmd, direction = 'float'}) end
+for cmd, func in pairs({
+    [1] = function() floater('lazygit'):toggle() end, -- git
+    [2] = ':Neoformat<CR> :w<CR>', -- format
+    [3] = function() floater('ddgr --rev'):toggle() end, -- web search
+    [4] = function() floater('oil'):toggle() end, -- web bookmarks
+    [5] = function() floater('nap'):toggle() end -- notes
+}) do vim.keymap.set('n', '<F' .. cmd .. '>', func) end
 
 -- LEADER SHORTCUTS
 for cmd, func in pairs({
-    k = 'move-2<CR>==', -- shift line down
-    j = 'move+<CR>==', -- shift line up
     h = 'noh<CR>', -- clear highlighting
-    t = 'Texplore<CR>', -- open netrw in new tab
+    j = 'move+<CR>==', -- shift line up
+    k = 'move-2<CR>==', -- shift line down
     o = 'Hexplore<CR>', -- open netrw in horizontal pane
+    t = 'Texplore<CR>', -- open netrw in new tab
     v = 'Vexplore<CR>' -- open netrw in vertical pane
 }) do vim.keymap.set('n', '<leader>' .. cmd, ':' .. func) end
 
@@ -43,15 +30,16 @@ local t = require('telescope')
 local tb = require('telescope.builtin')
 for cmd, func in pairs({
     b = t.extensions.file_browser.file_browser,
+    d = ':DevdocsOpenCurrentFloat<CR>',
     f = tb.find_files,
+    g = tb.live_grep,
+    h = tb.jumplist,
+    j = ':TodoTelescope<CR>',
+    m = tb.man_pages,
     o = tb.oldfiles,
+    s = tb.spell_suggest,
     t = tb.treesitter,
     w = tb.current_buffer_fuzzy_find,
-    g = tb.live_grep,
-    s = tb.spell_suggest,
-    m = tb.man_pages,
-    j = ':TodoTelescope<CR>',
-    d = ':DevdocsOpenCurrentFloat<CR>'
 }) do vim.keymap.set('n', telescope_prefix .. cmd, func) end
 
 -- TODO: add lsp leader shortcuts
